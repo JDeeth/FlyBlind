cf4f0e10ccddd5b473f281f1255a15d219cb73b9
Jack Deeth <git@jackdeeth.org.uk>
2018-08-05 19:54:35 +0100

    Restructure and rename

---
+++ README.md
@@ -0,0 +1,21 @@
+FlyBlind
+========
+
+This X-Plane plugin disables drawing of the outside world.
+
+ - Boost framerates
+ - Fly IFR
+
+When the command is triggered, the plugin registers drawing callbacks for most
+of X-Plane's scenery rendering phases. The callback renders a solid colour
+instead of scenery.
+
+Lights are still rendered, including the sun at some angles. Disabling the
+drawing phase where they're rendered also causes visual glitches.
+
+Compiled using Philipp Ringler's Plugin Patterns Library, as modified by me.
+This is included as a git submodule.
+
+Project is arranged to be built using qmake to compile PPL as a static library.
+
+Developed on Linux for X-Plane 10.
--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -1,72 +1,81 @@
+/// (c) Jack Deeth 2018 - see attached LICENSE and README.md
+
 #include <XPLMPlugin.h>
+
 #include <cstring>
-#include <string>
 #include <memory>
+#include <string>
 
 #include <GL/glew.h>
 
-#include <XPLMGraphics.h>
 #include <XPLMDisplay.h>
+#include <XPLMGraphics.h>
 
-#include <dataref.h>
 #include <command.h>
+#include <dataref.h>
 
-class FlyBlind {
+class StopSceneryRendering {
+  // When created, stops most of X-Plane's scenery from being rendered. Instead
+  // of drawing scenery, it draws a solid colour based on the ambient light.
+  // When destroyed, restores normal X-Plane scenery rendering.
+  static int clear_(int, int, void*);
   static PPL::DataRef<float> outside_r_;
   static PPL::DataRef<float> outside_g_;
   static PPL::DataRef<float> outside_b_;
-  static int clear_(int, int, void*);
 
-  class MakeBlank {
-  public:
-    MakeBlank() {
-      XPLMRegisterDrawCallback(clear_, xplm_Phase_Airports, 1, nullptr);
-      XPLMRegisterDrawCallback(clear_, xplm_Phase_Terrain, 1, nullptr);
-      XPLMRegisterDrawCallback(clear_, xplm_Phase_Vectors, 1, nullptr);
-    }
-    ~MakeBlank() {
-      XPLMUnregisterDrawCallback(clear_, xplm_Phase_Airports, 1, nullptr);
-      XPLMUnregisterDrawCallback(clear_, xplm_Phase_Terrain, 1, nullptr);
-      XPLMUnregisterDrawCallback(clear_, xplm_Phase_Vectors, 1, nullptr);
-    }
-  };
-
-  std::unique_ptr<MakeBlank> blank_{nullptr};
-
-  PPL::Command toggle_{"Albair/FlyBlind/toggle",
-                       "Make the world go away",
-                       [this](PPL::Command::Phase phase) {
-                         if (phase == PPL::Command::Phase::Begin) {
-                           if (blank_ == nullptr)
-                             blank_ = std::make_unique<MakeBlank>();
-                           else
-                             blank_.reset();
-                         }
-                         return PPL::Command::Outcome::Halt;
-                       }};
+public:
+  StopSceneryRendering() {
+    // disabling xplm_Phase_FirstScene hides lights, but causes glitches
+    XPLMRegisterDrawCallback(clear_, xplm_Phase_Objects, 1, nullptr);
+    XPLMRegisterDrawCallback(clear_, xplm_Phase_Airports, 1, nullptr);
+    XPLMRegisterDrawCallback(clear_, xplm_Phase_Terrain, 1, nullptr);
+    XPLMRegisterDrawCallback(clear_, xplm_Phase_Vectors, 1, nullptr);
+  }
+  ~StopSceneryRendering() {
+    XPLMUnregisterDrawCallback(clear_, xplm_Phase_Objects, 1, nullptr);
+    XPLMUnregisterDrawCallback(clear_, xplm_Phase_Airports, 1, nullptr);
+    XPLMUnregisterDrawCallback(clear_, xplm_Phase_Terrain, 1, nullptr);
+    XPLMUnregisterDrawCallback(clear_, xplm_Phase_Vectors, 1, nullptr);
+  }
 };
 
-PPL::DataRef<float> FlyBlind::outside_r_{
-    "sim/graphics/misc/outside_light_level_r"};
-PPL::DataRef<float> FlyBlind::outside_g_{
-    "sim/graphics/misc/outside_light_level_g"};
-PPL::DataRef<float> FlyBlind::outside_b_{
-    "sim/graphics/misc/outside_light_level_b"};
-
-int FlyBlind::clear_(int, int, void*) {
+int StopSceneryRendering::clear_(int, int, void*) {
   glClearColor(outside_r_ * 0.75f, outside_g_ * 0.75f, outside_b_ * 0.75f, 1);
   glClear(GL_COLOR_BUFFER_BIT);
   return 0;
 }
 
+PPL::DataRef<float> StopSceneryRendering::outside_r_{
+    "sim/graphics/misc/outside_light_level_r"};
+PPL::DataRef<float> StopSceneryRendering::outside_g_{
+    "sim/graphics/misc/outside_light_level_g"};
+PPL::DataRef<float> StopSceneryRendering::outside_b_{
+    "sim/graphics/misc/outside_light_level_b"};
+
+class FlyBlind {
+  // When the command is triggered, either creates or destroys a
+  // StopSceneryRendering object stored in the unique_ptr
+  std::unique_ptr<StopSceneryRendering> hide_world_{nullptr};
+  PPL::Command toggle_{
+      "Albair/FlyBlind/toggle",  // <<<< change this to rename command
+      "Make the world go away",
+      [this](PPL::Command::Phase phase) {
+        if (phase == PPL::Command::Phase::Begin) {
+          if (hide_world_ == nullptr)
+            hide_world_ = std::make_unique<StopSceneryRendering>();
+          else
+            hide_world_.reset();
+        }
+        return PPL::Command::Outcome::Halt;
+      }};
+};
+
 static std::unique_ptr<FlyBlind> flyblind;
 
 PLUGIN_API int XPluginStart(char* out_name, char* out_sig, char* out_desc) {
-  std::string name{"FlyBlind"};
-  strcpy(out_name, name.c_str());
-  strcpy(out_sig, name.c_str());
-  std::string desc{"Disables external drawing by command"};
-  strcpy(out_desc, desc.c_str());
+  strcpy(out_name, "FlyBlind");
+  strcpy(out_sig, "FlyBlind");
+  strcpy(out_desc, "Disables external drawing by command");
 
   flyblind = std::make_unique<FlyBlind>();
 
